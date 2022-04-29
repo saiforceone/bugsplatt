2f4f08c0aec50205511f26420b675a874bcc125c
saiforceone <saiforceone@gmail.com>
2022-04-29 00:52:36 -0500

    add client code. todo: update auth0 to append user data to JWT so api middleware can work

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,43 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useAuth0 } from "@auth0/auth0-react";
 import logo from './logo.svg'
 import './App.css'
+import LoginButton from './components/shared/LoginButton';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [accessToken, setAccessToken] = useState('');
+
+  const { user, isAuthenticated, getAccessTokenWithPopup } = useAuth0();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const token = await getAccessTokenWithPopup({
+          audience: 'https://bugsplatt-api.io',
+        });
+        console.log('token is: ', token);
+        setAccessToken(token);
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, [getAccessTokenWithPopup, isAuthenticated]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      {isAuthenticated && user && (
+        <div>
+          <img src={user.picture} alt={user.name} />
+          <h2>{user.nickname}</h2>
+        </div>
+      )}
+      <LoginButton />
     </div>
   )
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Auth0Provider } from "@auth0/auth0-react";
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      audience="https://bugsplatt-api.io"
+      domain="dev-whkdhipc.us.auth0.com"
+      clientId="mov6FS0c9rkGDgjTMe6QhxETUtSWH85Y"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 )
