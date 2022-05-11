94ec9a448d2d58c00cefffdc94c132a1f0f26152
saiforceone <saiforceone@gmail.com>
2022-05-10 22:54:24 -0500

    add tailwind basic setup

--- client/src/components/shared/LoginButton.tsx
+++ client/src/components/shared/LoginButton.tsx
@@ -4,7 +4,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return <button className="bg-blue-500 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded" onClick={() => loginWithRedirect()}>Log In</button>;
 };
 
 export default LoginButton;
