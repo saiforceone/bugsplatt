3b76cb3fec0c6be94fbda9b6f25751c40895d485
saiforceone <saiforceone@gmail.com>
2022-06-28 23:34:44 -0500

    update project filter functionality. todo: add backdrop to modal

--- client/src/components/PageComponents/ProjectFilter/ProjectFilter.tsx
+++ client/src/components/PageComponents/ProjectFilter/ProjectFilter.tsx
@@ -4,7 +4,7 @@ import { DefaultButton } from "../../BaseComponents/DefaultButton/DefaultButton"
 import { ModalHeaderProps } from "../../Modals/ModalHeader/ModalHeader";
 import { SelectableOptionModal } from "../../Modals/SelectableOptionModal/SelectableOptionModal";
 import { ProjectFilterOption } from "../ProjectFilterOption/ProjectFilterOption";
-import './ProjectFilter.css';
+import "./ProjectFilter.css";
 
 interface ProjectFilterProps {
   onFilter: () => void;
@@ -13,61 +13,154 @@ interface ProjectFilterProps {
   projectTypes: SelectableOption[];
 }
 
-type ModalTargetType = "user" | "team" | "project" | "date-range";
+type ModalTargetType = "user" | "team" | "project-type" | "date-range";
 
 const DefaultHeaderProps: ModalHeaderProps = {
-  title: 'Default Title',
-  onClose: () => {}
-}
+  title: "Default Title",
+  onClose: () => {},
+};
 
 export const ProjectFilter: FC<ProjectFilterProps> = ({
   onFilter,
   users,
   teams,
-  projectTypes
+  projectTypes,
 }) => {
-
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedUser, setSelectedUser] = useState<SelectableOption>();
   const [selectedTeam, setSelectedTeam] = useState<SelectableOption>();
   const [selectedProjType, setSelectedProjType] = useState<SelectableOption>();
 
-  const [modalHeaderProps, setModalHeaderProps] = useState<ModalHeaderProps>(DefaultHeaderProps);
+  const [modalHeaderProps, setModalHeaderProps] =
+    useState<ModalHeaderProps>(DefaultHeaderProps);
   const [modalOptions, setModalOptions] = useState<SelectableOption[]>([]);
+  const [selectedTarget, setSelectedTarget] =
+    useState<ModalTargetType | undefined>(undefined);
 
   // placeholder
-  const onShowModal = useCallback((target: ModalTargetType) => {
-    // set up the props necessary for the Selectable Option Modal
-    // - model header props, options
-  }, []);
+  const onShowModal = useCallback(
+    (target: ModalTargetType) => {
+      // set up the props necessary for the Selectable Option Modal
+      // - model header props, options
+      const onClose = () => setModalVisible(false);
+      let options: SelectableOption[] = [];
+      let title;
+      switch (target) {
+        case "user":
+          title = "Choose User";
+          options = users;
+          break;
+        case "project-type":
+          title = "Choose Project";
+          options = projectTypes;
+          break;
+        case "team":
+          title = "Choose Team";
+          options = teams;
+          break;
+        default:
+          title = "";
+          break;
+      }
+      setModalHeaderProps({
+        title,
+        onClose,
+      });
+      setModalOptions(options);
+      setModalVisible(true);
+      setSelectedTarget(target);
+    },
+    [modalHeaderProps]
+  );
 
   // placeholder
-  const onOptionSelected = useCallback((target: ModalTargetType) => {
-    switch (target) {
-      // fill out implementation
-      default: 
+  const onOptionSelected = useCallback(
+    (option: SelectableOption) => {
+      switch (selectedTarget) {
+        // fill out implementation
+        case "user":
+          setSelectedUser(option);
+          break;
+        case "project-type":
+          setSelectedProjType(option);
+          break;
+        case "team":
+          setSelectedTeam(option);
+          break;
+        default:
+          break;
+      }
+      setModalVisible(false);
+    },
+    [selectedTarget, selectedUser, selectedProjType, selectedTeam]
+  );
+
+  const onResetOption = useCallback(() => {
+    switch (selectedTarget) {
+      case "user":
+        setSelectedUser(undefined);
+        break;
+      case "team":
+        setSelectedTeam(undefined);
+        break;
+      case "project-type":
+        setSelectedProjType(undefined);
+        break;
+      default:
         break;
     }
+  }, [selectedTarget, selectedTeam, selectedUser, selectedTarget]);
+
+  const onApplyFilter = useCallback(() => {
+    // TODO: complete implementation
+  }, [selectedUser, selectedTeam, selectedProjType])
+
+  const onResetAllOptions = useCallback(() => {
+    // TODO: complete implementation
   }, []);
 
   return (
     <div className="project-filter">
       <div className="project-filter--controls">
-        <ProjectFilterOption extraCss="mr-1" label="User" resetAction={() => {}} />
-        <ProjectFilterOption extraCss="mr-1" label="Team" resetAction={() => {}} />
-        <ProjectFilterOption extraCss="mr-1" label="Type" resetAction={() => {}} />
-        <ProjectFilterOption extraCss="mr-1" label="Date" resetAction={() => {}} />
+        <ProjectFilterOption
+          extraCss="mr-1"
+          label="User"
+          resetAction={onResetOption}
+          onClick={() => onShowModal("user")}
+          value={selectedUser}
+        />
+        <ProjectFilterOption
+          extraCss="mr-1"
+          label="Team"
+          resetAction={onResetOption}
+          onClick={() => onShowModal("team")}
+          value={selectedTeam}
+        />
+        <ProjectFilterOption
+          extraCss="mr-1"
+          label="Type"
+          resetAction={onResetOption}
+          onClick={() => onShowModal("project-type")}
+          value={selectedProjType}
+        />
+        <ProjectFilterOption
+          extraCss="mr-1"
+          label="Date"
+          resetAction={() => {}}
+        />
       </div>
-      <div className='default-actions-container'>
-        <DefaultButton active label='Apply' />
-        <DefaultButton active label='Reset' />
+      <div className="default-actions-container">
+        <DefaultButton active label="Apply" onClick={onApplyFilter} />
+        <DefaultButton active label="Reset" onClick={onResetAllOptions} />
       </div>
       <SelectableOptionModal
-        onSelectOption={(opt) => {}}
+        onSelectOption={(opt) => {
+          onOptionSelected(opt);
+        }}
         modalHeaderProps={modalHeaderProps}
         options={modalOptions}
         visible={modalVisible}
       />
     </div>
-  )
-}
+  );
+};
--- client/src/components/PageComponents/ProjectFilterOption/ProjectFilterOption.tsx
+++ client/src/components/PageComponents/ProjectFilterOption/ProjectFilterOption.tsx
@@ -1,30 +1,40 @@
 import { FC, useState } from "react";
 import { HiX } from "react-icons/hi";
+import { SelectableOption } from "../../../interfaces";
 import { IconButton } from "../../BaseComponents/IconButton/IconButton";
 import "./ProjectFilterOption.css";
 
 interface ProjectFilterOptionProps {
+  onClick?: () => void;
   resetAction: () => void;
   label: string;
-  value?: string;
+  value?: SelectableOption;
   extraCss?: string;
 }
 
 export const ProjectFilterOption: FC<ProjectFilterOptionProps> = ({
+  onClick,
   resetAction,
   label,
   value,
-  extraCss
+  extraCss,
 }) => {
-  
+  console.log('value: ', value);
   return (
-    <div className={["proj-filter-opt", extraCss].join(' ')}>
-      <div className="proj-filter-opt--content">
+    <div className={["proj-filter-opt", extraCss].join(" ")}>
+      <div className="proj-filter-opt--content" onClick={onClick}>
         <div className="proj-filter-opt--label">
           <span className="proj-filter-opt--label-content">{label}</span>
         </div>
         <div className="proj-filter-opt--value">
-          <p className={["proj-filter-opt--value-content", value ? '' : 'text-gray-400'].join(' ')}>{value ? value : 'Choose...'}</p>
+          <p
+            className={[
+              "proj-filter-opt--value-content",
+              value ? "" : "text-gray-400",
+            ].join(" ")}
+          >
+            {value ? value.label : "Choose..."}
+          </p>
         </div>
       </div>
       <IconButton
@@ -33,7 +43,6 @@ export const ProjectFilterOption: FC<ProjectFilterOptionProps> = ({
         icon={<HiX className="default-icon self-center" />}
         onClick={resetAction}
       />
-
     </div>
   );
 };
