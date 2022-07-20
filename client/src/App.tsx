6e6e1b874e917f5b5636cb1e94c32f9f9ecc7531
saiforceone <saiforceone@gmail.com>
2022-07-19 22:02:44 -0500

    clean up app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,15 @@
 import { useEffect, useState } from 'react'
 import { useAuth0 } from "@auth0/auth0-react";
-import { useDispatch, useSelector } from 'react-redux';
 import './App.css'
 import LoginButton from './components/shared/LoginButton';
 import LogoutButton from './components/shared/LogoutButton';
 import { ProjectModal } from './components/Modals/ProjectModal/ProjectModal';
-import { useLazyGetCommentsQuery } from './data/rtkApis/commentApi';
-import { useLazyGetProjectsQuery, useLazyGetProjectWithIdQuery } from './data/rtkApis/projectApi';
-import { useLazyGetIssuesQuery, useUpdateIssueMutation, useAddIssueMutation, useDeleteIssueMutation } from './data/rtkApis/issueApi';
-import {setAuthToken} from './data/slices/authSlice';
-import {useAppSelector} from './data/hooks/useAppSelector'
 
 function App() {
   const [accessToken, setAccessToken] = useState('');
   const [modalVisible, setModalVisible] = useState(false);
 
-  // TEMP CODE
-  const dispatch = useDispatch();
-  const authStore = useAppSelector((state) => state.auth);
-
   const { user, isAuthenticated, getAccessTokenWithPopup } = useAuth0();
-  // TODO: need a way to force waiting for the token before executing queries
-  const [trigger, result, lastPromiseInfo] = useLazyGetCommentsQuery();
-  const [triggerProjects] = useLazyGetProjectsQuery();
-  const [triggerSingleProject] = useLazyGetProjectWithIdQuery()
-  const [triggerUpdateIssue, {isLoading: isUpdatingIssue}] = useUpdateIssueMutation();
-  const [triggerAddIssue, {isLoading: isCreatingIssue}] = useAddIssueMutation();
-  const [triggerDeleteIssue] = useDeleteIssueMutation();
-  // END TEMP CODE
-
 
   useEffect(() => {
     (async () => {
@@ -43,46 +24,6 @@ function App() {
       }
     })();
   }, [getAccessTokenWithPopup, isAuthenticated]);
-
-  /**
-   * TEMPORARY CODE
-   */
-  useEffect(() => {
-    dispatch(setAuthToken(accessToken));
-  }, [accessToken])
-
-  useEffect(() => {
-    console.log('auth token in redux set');
-    console.log('authStore:', authStore);
-    if (authStore.authToken) {
-      trigger();
-      // triggerProjects();
-      triggerSingleProject("6275ee2c0e2d632d52abcf7d");
-
-      triggerUpdateIssue({_id: "62d0e57c597f82d81c84afcb", expectedCloseDate: "2022-08-04"});
-      // triggerAddIssue({
-      //   "createdBy": "622c0d168787b80069099f79",
-      //   "assignedTo": "622c0d168787b80069099f79",
-      //   "associatedProject": "6275ede70e2d632d52abcf6c",
-      //   "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      //   "expectedCloseDate": "2022-08-01T00:00:00.000Z",
-      //   "priority": "omg-wtf",
-      //   "status": "active",
-      //   "tags": [
-      //       "tag 1",
-      //       "tag 2"
-      //   ],
-      //   "title": "From RTK Query Post",
-      //   "watchedBy": [
-      //       "622c0d168787b80069099f79"
-      //   ],
-      // })
-      // triggerDeleteIssue("62d0e57b597f82d81c84afc6");
-    }
-  }, [authStore.authToken]);
-  /**
-   * END TEMP CODE
-   */
 
   return (
     <div className="App">
