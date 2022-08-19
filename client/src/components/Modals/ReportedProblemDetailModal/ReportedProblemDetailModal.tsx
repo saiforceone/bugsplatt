778159cf839327b7c5889b09b1563d18b35d99ff
sai <saiforceone@gmail.com>
2022-08-19 18:26:34 -0500

    update api for reported problem

--- client/src/components/Modals/ReportedProblemDetailModal/ReportedProblemDetailModal.tsx
+++ client/src/components/Modals/ReportedProblemDetailModal/ReportedProblemDetailModal.tsx
@@ -19,11 +19,24 @@ export const ReportedProblemDetailModal: FC<ReportedProblemDetailModalProps> = (
   return (
     <ModalWrapper modalHeaderProps={modalHeaderProps} visible={visible}>
       <div className="modal--container">
-        <StackedLabel label="Reported By" content={`${problem.createdBy.firstName} ${problem.createdBy.lastName}`} />
-        <StackedLabel label="Reported On" content={FormattingUtils.formatDate(problem.createdAt, DATE_FORMATS['MEDIUM_DATE_TIME'])} />
-        <StackedLabel label="Description of what happened" content={problem.content} />
-        <StackedLabel label="Type of Problem" content={problem.problemType} />
-        <StackedLabel label="Problem Status" content={problem.problemStatus} />
+        <StackedLabel label="Reported By" content={`${problem.createdBy.firstName} ${problem.createdBy.lastName}`}/>
+        <StackedLabel
+          label="Reported On"
+          content={FormattingUtils.formatDate(problem.createdAt, DATE_FORMATS['MEDIUM_DATE_TIME'])}
+        />
+        <StackedLabel
+          label="Description of what happened" content={problem.content}
+        />
+        <StackedLabel
+          label="Type of Problem" content={problem.problemType}
+        />
+        <StackedLabel
+          label="Problem Status" content={problem.problemStatus}
+        />
+        <StackedLabel
+          label="Last Updated"
+          content={FormattingUtils.formatDate(problem.updatedAt, DATE_FORMATS['MEDIUM_DATE_TIME'])}
+        />
       </div>
     </ModalWrapper>
   );
